2216fbaf6239a1638bf0f644182caf00ac822aed
vinitha <selvivini@gmail.com>
2021-03-09 12:00:52 -0600

    added dotenv and created the server

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Set up MySQL connection.
+const mysql = require('mysql');
+const dotEnv = require('dotenv');
+
+dotEnv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  // NOTE: Be sure to add your MySQL password here!
+  password: process.env.DB_PASS,
+  database: 'burgers_db',
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
