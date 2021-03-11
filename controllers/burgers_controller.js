561bb73722d4e388bc29afdde4988f3854661b14
vinitha <selvivini@gmail.com>
2021-03-11 10:41:45 -0600

    added routes for post and update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,6 +14,24 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.post('/api/burgers', (req,res)=>{
+   burger.insertOne(['burger_name'],[req.body.burger_name],(result)=>{
+     res.json({id: result.insertId})
+   });
+  });
+
+  router.put('/api/burgers/:id',(req,res)=>{
+    const  condition = `id= ${req.params.id}` 
+    console.log('condition', condition)
+    burger.updateOne({devoured: req.body.devoured},condition, (result) =>{
+      
+      if(result.changedRows === 0){
+        return res.status(404).end()
+      }
+      res.status(200).end()
+    } )
+  })
+
 
 
 
