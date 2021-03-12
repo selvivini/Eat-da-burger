be6f7f66872d5e4eb01edbaa487ca92bcb9b3b63
vinitha <selvivini@gmail.com>
2021-03-11 19:17:58 -0600

    modified the schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers(
   id INT NOT NULL AUTO_INCREMENT,
-  burger_name NOT NULL VARCHAR(60),
+  burger_name  VARCHAR(60) NOT NULL ,
   devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
